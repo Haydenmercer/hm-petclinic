43bb3f4bf163023f7867c4ad4ff9dc35f87c7051
HaydenM <haydenmercer@msn.com>
2021-10-14 20:59:08 +0100

    Imported Spring Starter project

---
+++ src/main/java/com/hayden/hmpetclinic/HmPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.hayden.hmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmPetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hayden/hmpetclinic/HmPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayden.hmpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
