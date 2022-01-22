0bb7ad48743130b6db9657e2c88a5553c59521ff
HaydenM <haydenmercer@msn.com>
2022-01-22 17:33:15 +0000

    Added Vet JPA Service. Closes #41.

---
+++ pet-clinic-data/src/main/java/com/hayden/hmpetclinic/services/jpa/VetJPAService.java
@@ -0,0 +1,48 @@
+package com.hayden.hmpetclinic.services.jpa;
+
+import com.hayden.hmpetclinic.model.Vet;
+import com.hayden.hmpetclinic.repositories.VetRepository;
+import com.hayden.hmpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile(value = "jpa")
+public class VetJPAService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJPAService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findByID(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet save(Vet obj) {
+        return vetRepository.save(obj);
+    }
+
+    @Override
+    public void delete(Vet obj) {
+        vetRepository.delete(obj);
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
