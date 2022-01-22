0d646f33faa0b46745755937bb5721bdba9f43f2
HaydenM <haydenmercer@msn.com>
2022-01-22 16:38:14 +0000

    Conerts Visit and Pet to JPA Entities

--- pet-clinic-data/src/main/java/com/hayden/hmpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/hayden/hmpetclinic/model/Pet.java
@@ -4,6 +4,8 @@ import org.springframework.jmx.export.annotation.ManagedAttribute;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -19,6 +21,17 @@ public class Pet extends NamedEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public PetType getPetType() {
         return petType;
